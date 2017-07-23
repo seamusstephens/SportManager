06bd961c2df5fba4381f988e98026b64f8810c32
Seamus <seamusstephens@gmail.com>
2017-07-24 00:11:48 +1000

    fix

--- main/main.go
+++ main/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"net/http"
+	"log"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	port := flag.String("p", "8000", "port")
+	dir := flag.String("d", ".", "dir")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	log.Printf("Serving %s on Http port: %s\n", *dir, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
